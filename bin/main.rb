aa216d4d620076479c74caeb36508e8d885273d8
Xarri Jorge <kodinglife@gmail.com>
2019-06-08 13:29:09 +0000

    Tweaked logic and interface to board and gameplay

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,11 @@
 
-require_relative 'board'
-require_relative 'player'
-require_relative 'game_logic'
+require_relative '../lib/board'
+require_relative '../lib/player'
+require_relative '../lib/game_logic'
 
 
 board= Board.new
 user_one = Player.new('user_one','O').name
 user_two = Player.new('user_two','T').name
 
-GameLogic.new(user_one,user_two,board).game_play
+GameLogic.new(user_one, user_two, board).game_play
--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -14,89 +14,26 @@ class GameLogic
         @board.display
         puts "enter username for player_one"
         @player_one=gets.chomp.to_s
-        puts "#{@player_one} enter symbol for identifying you on the game"
+        puts "#{@player_one}, please enter symbol for identifying you on the board"
         user_id_1=gets.chomp.to_s.upcase
         puts "enter username for player_two"
         @player_two=gets.chomp.to_s
-        puts "#{@player_two} enter symbol for identifying you on the game"
+        puts "#{@player_two}, please enter symbol for identifying you on the board"
         user_id_2=gets.chomp.to_s.upcase
 
         6.times do | a |
           @board.display
-          puts "select an option form 1...9"
+          current_player = a.even? ? @player_one : @player_two
+          puts "#{current_player}, select an option form 1...9"
           option=gets.chomp.to_i
-
+          character = (a.even? ? user_id_1 : user_id_2).to_s
           
-            if( option == 1 )
-              if a.even? 
-                 @board.post[0] = "#{user_id_1}"
-              else
-                @board.post[0] = "#{user_id_2}"
-              end     
-            end
-            if( option == 2 )
-              if a.even? 
-                 @board.post[1] = "#{user_id_1}"
-              else
-                 @board.post[1] = "#{user_id_2}"
-              end  
-            end
-            if( option == 3)
-              if a.even? 
-                 @board.post[2] = "#{user_id_1}"
-              else
-                 @board.post[2] = "#{user_id_2}"
-              end  
-            end
-            if( option == 4 )
-               if a.even? 
-                 @board.post[3] = "#{user_id_1}"
-               else
-                 @board.post[3] = "#{user_id_2}"
-               end  
-            end
-            if( option == 5 )
-               if a.even? 
-                 @board.post[4] = "#{user_id_1}"
-               else
-                 @board.post[4] = "#{user_id_2}"
-               end  
-            end
-            if( option == 6 )
-               if a.even? 
-                 @board.post[5] = "#{user_id_1}"
-               else
-                 @board.post[5]= "#{user_id_2}"
-               end  
-            end
-            if( option == 7 )
-               if a.even? 
-                 @board.post[6] = "#{user_id_1}"
-               else
-                 @board.post[6] = "#{user_id_2}"
-               end 
-            end
-            if( option == 8 )
-               if a.even? 
-                 @board.post[7] = "#{user_id_1}"
-               else
-                 @board.post[7] = "#{user_id_2}"
-               end  
-            end
-            if( option == 9)
-               if a.even? 
-                 @board.post[8] = "#{user_id_1}"
-               else
-                 @board.post[8]= "#{user_id_2}"
-               end  
-            end
-
-        @@options.push(option)
+          @board.post[option-1] = character if @board.post.include? option
+          @@options.push(option)
         end
       @board.display        
     end
 
-    
     def valid_move ( input )
         return "out of range" if input > 9 || input < 0
         return "number was already selected " if @@options.include?( input )
