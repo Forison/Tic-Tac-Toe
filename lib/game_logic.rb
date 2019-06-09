511b57aca485fffdbd51ea09a62f5aeb62c3f4c2
Xarri Jorge <kodinglife@gmail.com>
2019-06-09 09:46:57 +0000

    Minor changes to game_play

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,11 +1,20 @@
+require 'set'
+
 class GameLogic
-    @@WINS=[ [1,2,3],[1,4,7],[1,5,9],[2,5,8],[3,6,9],[3,5,7],[4,5,6],[7,8,9] ]
+    @@WINS=[[1,2,3],[1,4,7],[1,5,9],[2,5,8],[3,6,9],[3,5,7],[4,5,6],[7,8,9]]
     @@options=[]
+
     # operate based on moves made by player 
     def initialize(player_one,player_two,board)
         @player_one=player_one
         @player_two=player_two
         @board=board
+    end
+
+    def valid_move? ( input )
+        return "out of range" if input > 9 || input < 0
+        return "number was already selected " if @@options.include?( input )
+        true
     end
 
     def board_full?
@@ -15,7 +24,9 @@ class GameLogic
     end
 
     def winner_found
-
+      @wins.each do |moves|
+        puts "#{current_player} wins!" if Set.new(moves).length == 1;
+      end
     end
 
     def game_play
@@ -29,30 +40,28 @@ class GameLogic
         user_id_2=@player_two[0].to_s.upcase
 
         a = 0
-        loop do
+        until board_full?
           @board.display
-
-          if board_full?
-            puts "The board is full"
-            puts "STALEMATE!"
-            return
-           end
           
           current_player = a.even? ? @player_one : @player_two
           puts "#{current_player}, select an option form 1...9"
-          option=gets.chomp.to_i
+          option=gets.chomp.to_i 
           character = (a.even? ? user_id_1 : user_id_2).to_s
           
+
+          @@WINS = @@WINS.each do |moves|
+            if moves.include? option
+              moves[option] = character
+            end
+          end
           @board.post[option-1] = character if @board.post.include? option
+          
           @@options.push(option)
           a +=1
+          puts @@WINS
         end
-      @board.display        
-    end
-
-    def valid_move ( input )
-        return "out of range" if input > 9 || input < 0
-        return "number was already selected " if @@options.include?( input )
-        true
+      @board.display  
+      puts "board full!"
+      puts "STALEMATE!"
     end
 end
